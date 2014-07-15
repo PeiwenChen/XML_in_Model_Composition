ead06d6beef4954cd4fa0e386e327815d9006b46
Peiwen Chen <page.peiwenchen@gmail.com>
2014-07-14 23:30:16 -0400

    some modifications about string/list handling

--- tagsignature.py
+++ tagsignature.py
@@ -30,47 +30,45 @@ class TagSignature:
 	self.tagtype = document.find('TagType').text
 	self.nametag = document.find('NameTag').text.split(',')
 
-	ilist = []
+	self.inputPorts = []
 	for port in document.findall('Inputs/Port'): 
 	    ### create a new Port 
             newport = Port(port.find('PortName').text.split(','), port.find('Type').text.split(','), port.find('Description').text.split(','))
-	    ilist.append(newport)
-	self.inputPorts = ilist 
+	    self.inputPorts.append(newport)
 	
-	olist = []
+	self.outputPorts = []
 	for port in document.findall('Outputs/Port'):
-	     port2add = Port(port.find('PortName').text, port.find('Type').text, port.find('Description').text)
-	     olist.append(port2add)
-	self.outputPorts = olist
+	     port2add = Port(port.find('PortName').text.split(','), port.find('Type').text.split(','), port.find('Description').text.split(','))
+	     self.outputPorts.append(port2add)
 
     def saveTagSignatureToXML(self, xmldir):
 	tagsignature = Element('TagSignature')
 	tagtype = SubElement(tagsignature, 'TagType')  
 	tagtype.text = str(self.tagtype)  
 	nametag = SubElement(tagsignature, 'NameTag')
-	nametag.text = str(self.nametag)   
+	nametag.text = str(','.join(self.nametag))   
 	
 	### Inputs Ports
 	inputs = SubElement(tagsignature, 'Inputs')
 	for port in self.inputPorts: ### for each port in the inputs Port List
 	    inputport = SubElement(inputs, 'Port')  
 	    inputportname = SubElement(inputport, 'PortName')
-	    inputportname.text = str(port.PortName) ###add PortName value
+	    inputportname.text = str(','.join(port.PortName)) ###add PortName value
 	    inputporttype = SubElement(inputport, 'Type')
-	    inputporttype.text = str(port.Type) ###add Type value
+	    inputporttype.text = str(','.join(port.Type)) ###add Type value
 	    inputportdes = SubElement(inputport, 'Description')
-	    inputportdes.text = str(port.Description) ###add des value
+	    inputportdes.text = str(','.join(port.Description)) ###add des value
 	
 	### Outputs Ports
 	outputs = SubElement(tagsignature, 'Outputs')
 	for port in self.outputPorts: ### for each port in the inputs Port List
 	    outputport = SubElement(outputs, 'Port')  
 	    outputportname = SubElement(outputport, 'PortName')
-	    outputportname.text = str(port.PortName) ###add PortName value
+	    outputportname.text = str(','.join(port.PortName)) ###add PortName value
 	    outputporttype = SubElement(outputport, 'Type')
-	    outputporttype.text = str(port.Type) ###add Type value
+	    outputporttype.text = str(','.join(port.Type)) ###add Type value
 	    outputportdes = SubElement(outputport, 'Description')
-	    outputportdes.text = str(port.Description) ###add des value
+	    outputportdes.text = str(','.join(port.Description)) ###add des value
 	
 	###write into xml file
 	output_file = open(xmldir, 'w')
