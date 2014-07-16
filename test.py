c10042a1d1592521b139e010cf7c2fccfec6419d
Peiwen Chen <page.peiwenchen@gmail.com>
2014-07-16 00:14:15 -0400

    add testcase func

--- test.py
+++ test.py
@@ -1,21 +1,42 @@
 import tagsignature
 import tagsignaturelist
+import modes
+import modeslist
+
+def testcase1():
+    print "----------- testing class TagSignature----------------\n"
+    tagsi = tagsignature.TagSignature()
+    tagsi.loadTagSignatureFromXML('tagsignature.in.xml')
+    tagsi.saveTagSignatureToXML('tagsignature.out.xml')
+    print "---------test done  ! -----------------------------------\n"
+
+def testcase2():
+    print "------------- testing class TagSignatureList! ---------\n"
+    tagsilist = tagsignaturelist.TagSignatureList()
+    tagsilist.loadTagSignatureListFromXML('tagsignaturelist.in.xml')
+    tagsilist.saveTagSignatureListToXML('tagsignaturelist.out.xml')
+    print "------------- test done ! ---------------------------------\n"
+
+def testcase3():
+    print "---------------- testing for readline_tag-------------\n"""
+    tagsilist = tagsignaturelist.TagSignatureList()
+    tagsilist.readTagSignatureListFromFile('TagSignature_file.txt')
+    print "----------------test done !------------------------------------\n"
+
+def testcase4():
+    print "-------------testing class ModelDescriptionList! ---------\n"
+    modeslist = ModelDescriptionList()
+    modeslist.loadModelDescriptionListFromXML('modelDescriptionList.in.xml')
+    modeslist.saveModelDescriptionListToXML('modelDescriptionList.out.xml')
+    print "-------------test done !-----------------------------------------\n"
+
+def testcase5():
+    print "------------test ModelDescriptionList read file.txt--------------\n"
+    modeslist = ModelDescriptionList()
+    modeslist.readModelDescriptionListFromFile('ModelDescription_file.txt')
+    print "-------------test done !-------------------------------------------\n"
 
 
-print "-----------I am doing test class TagSignature----------------\n"
 
-tagsi = tagsignature.TagSignature()
-tagsi.loadTagSignatureFromXML('tagsignature.in.xml')
-tagsi.saveTagSignatureToXML('tagsignature.out.xml')
-
-
-print "-------------I am doing test class TagSignatureList! ---------\n"
-tagsilist = tagsignaturelist.TagSignatureList()
-tagsilist.loadTagSignatureListFromXML('tagsignaturelist.in.xml')
-tagsilist.saveTagSignatureListToXML('tagsignaturelist.out.xml')
-
-
-
-""" I am doing test for readline_tag"""
-tagsilist.readTagSignatureListFromFile('TagSignature_file.txt')
+""" which testcase do you want to run ?"""
 
